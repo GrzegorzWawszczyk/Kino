238d87328e4c69ab7e87e47605856ea0ff25248a
Vadim Pyl <wpylypowycz@wi.zut.edu.pl>
2016-08-03 21:36:46 +0200

    Vadim's  first commit

--- src/main/java/org/zut/pbai/HomeController.java
+++ src/main/java/org/zut/pbai/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		//formattedDate
+		//formattedDateasdads
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
